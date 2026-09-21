6a3b24218e3ec50ceebb518121f2e4e186b94204
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-21 12:00:35 +0200

    Exercice 3 réalisé

--- src/net/lecnam/ecue2/tp3/Rectangle.java
+++ src/net/lecnam/ecue2/tp3/Rectangle.java
@@ -5,4 +5,18 @@ public class Rectangle {
     double longueur, largeur;
     Point pointOrigine = new Point(1,1);
 
+    /*Constructeur 1*/
+    public Rectangle(Point p, double longueur, double largeur){
+        this.pointOrigine = p;
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
+    /*Constructeur 2*/
+    public Rectangle(double pointX, double pointY, double longueur, double largeur){
+        this.pointOrigine = new Point(pointX,pointY);
+        this.longueur = longueur;
+        this.largeur = largeur;
+    }
+
 }
