7286cd58ea7faa8565723d4002ff28440c61c31a
Valentin Dujour <valentin.dujour@icloud.com>
2026-09-22 10:17:06 +0200

    Exercice 11 réalisé !

--- src/net/lecnam/ecue2/tp3/Exec.java
+++ src/net/lecnam/ecue2/tp3/Exec.java
@@ -3,39 +3,37 @@ package net.lecnam.ecue2.tp3;
 public class Exec  {
 
     public static void main(String[] args) {
-        Point p1 = new Point(0,0);
-
-        /*Utilisation de la méthode translate*/
-        p1.translate(1,1);
-
-        Point p2 = new Point(1,2);
-
-        /*Affichage true ou false de l'égalité des coordonnées*/
-        System.out.println(p1.equals(p2));
-
-        /*Utiliser la méthode toString pour afficher le point*/
-        System.out.println(p1.toString());
-
-        /*Utiliser la méthode retourneDistance pour afficher la distance entre 2 points*/
-        System.out.println("la distance est de : " + p1.retourneDistance(p2));
-
-        Rectangle r1 = new Rectangle(p1, 5, 5);
-        System.out.println("Le rectangle contient t il le point ? " + r1.contient(p2));
-
-        Rectangle r2 = new Rectangle(p2, 15, 15);
-        boolean rEquals = r1.equals(r2);
-        System.out.println(rEquals);
-
-        System.out.println(r1.toString());
-        System.out.println(r2.toString());
-
+        /*Créer un dessin*/
         Dessin d1 = new Dessin();
+
+        System.out.println("Dessin initialisé");
+
+        /*Créer plusieurs rectangles*/
+        Rectangle r1 = new Rectangle(1,2,4,6);
+        Rectangle r2 = new Rectangle(4,6,3,2);
+        Rectangle r3 = new Rectangle(0,3,5,7);
+
+        System.out.println("Les rectangles ont étés créé");
+
+        /*Mettre les rectangles dans le dessin*/
         d1.ajout(r1);
         d1.ajout(r2);
+        d1.ajout(r3);
 
-        System.out.println(d1.retourneSurface());
+        System.out.println("Les rectangles ont étés ajoutés au dessin !");
 
-        System.out.println(d1.retournePlusGrandRectangle());
+        /*Afficher la surface globale des rectangles*/
+        double surface = d1.retourneSurface();
+        System.out.println("La surface globale de tout les rectangles est : " + surface);
+
+        /*Afficher les informations du plus grand rectangle*/
+        System.out.println("Le plus grand rectangle du dessin est : " + d1.retournePlusGrandRectangle());
+
+        /*Déplacer les rectangles*/
+        d1.translate(5,2);
+
+        /*Afficher les informations du plus grand rectangle*/
+        System.out.println("Le plus grand rectangle du dessin est : " + d1.retournePlusGrandRectangle());
 
     }
 
